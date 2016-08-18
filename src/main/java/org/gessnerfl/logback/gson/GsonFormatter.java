b3333d48c883ba74aa0a2396af80e68eb530e526
Florian Gessner <flo.gessner@gmail.com>
2016-08-18 22:35:51 +0200

    Initial commit

---
+++ src/main/java/org/gessnerfl/logback/gson/GsonFormatter.java
@@ -0,0 +1,47 @@
+package org.gessnerfl.logback.gson;
+
+import java.io.IOException;
+import java.util.Map;
+
+import ch.qos.logback.contrib.json.JsonFormatter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonFormatter implements JsonFormatter {
+
+    public static final int BUFFER_SIZE = 512;
+
+    private Gson gson;
+    private boolean prettyPrint;
+
+    public GsonFormatter() {
+        this.gson = new Gson();
+        this.prettyPrint = false;
+    }
+
+    @Override
+    public String toJsonString(Map m) throws IOException {
+        return gson.toJson(m);
+    }
+
+    public Gson getGson() {
+        return gson;
+    }
+
+    public void setGson(Gson gson) {
+        this.gson = gson;
+    }
+
+    public boolean isPrettyPrint() {
+        return prettyPrint;
+    }
+
+    public void setPrettyPrint(boolean prettyPrint) {
+        this.prettyPrint = prettyPrint;
+        if(prettyPrint){
+        	gson = new GsonBuilder().setPrettyPrinting().create();
+        }
+    }
+}
+
---
+++ src/test/java/org/gessnerfl/logback/gson/GsonFormatterTest.java
@@ -0,0 +1,47 @@
+package org.gessnerfl.logback.gson;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class GsonFormatterTest {
+	
+	@Test
+	public void shouldCreateJsonForMap() throws Exception{
+		Map map = createTestData();
+		
+		GsonFormatter sut = new GsonFormatter();
+		
+		String json = sut.toJsonString(map);
+		
+		assertEquals("{\"array\":[\"foo\",\"bar\"],\"list\":[\"foo\",\"bar\"],\"number\":1,\"string\":\"foo\"}", json);
+	}
+	
+	@Test
+	public void shouldCreatePrettyJsonForMap() throws Exception{
+		Map map = createTestData();
+		
+		GsonFormatter sut = new GsonFormatter();
+		sut.setPrettyPrint(true);
+		
+		String json = sut.toJsonString(map);
+		
+		assertEquals("{\n  \"array\": [\n    \"foo\",\n    \"bar\"\n  ],\n  \"list\": [\n    \"foo\",\n    \"bar\"\n  ],\n  \"number\": 1,\n  \"string\": \"foo\"\n}", json);
+	}
+
+	private Map createTestData() {
+		Map<String,Object> data = new TreeMap<>();
+		data.put("array", new String[]{"foo", "bar"});
+		data.put("list", Lists.newArrayList("foo", "bar"));
+		data.put("number", 1L);
+		data.put("string", "foo");
+		return data;
+	}
+	
+}
